16ec18163d9a21b6d056b0b35484da699cb9d300
pedramktb <79080845+pedramktb@users.noreply.github.com>
2025-07-05 23:53:55 +0200

    feat: implement stack trace support

---
+++ CHANGELOG.md
@@ -0,0 +1,28 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](https://semver.org/).
+
+---
+
+## [Unreleased]
+
+### Added
+- `(*Err).WithStack()`: Adds the call site stack trace to the error.
+- `(*Err).Stack()`: Retrieves a formatted stack trace if available; supports optional custom formatting via `StackWithFormat` `StackOption`.
+
+---
+
+## [v1.1.0]  - 2025-07-04
+
+### Added
+- `(*Err).Unwrap()`: Implements the standard `Unwrap() error` interface, allowing integration with `errors.Unwrap()`. Supports nested unwrapping of single and multi-error types of length 2.
+
+---
+
+## [v1.0.0] - 2025-07-02
+
+### Added
+- Initial release
---
+++ stack_test.go
@@ -0,0 +1,35 @@
+package tagerr_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/pedramktb/go-tagerr"
+	"github.com/stretchr/testify/assert"
+)
+
+var stacklessError = &tagerr.Err{
+	Err:      errors.New("some error"),
+	Tag:      "some_error",
+	HTTPCode: 1000,
+}
+
+func stacklessErrorReturningFunction() error {
+	return stacklessError
+}
+
+func stackfullErrorerrorReturningFunction() error {
+	return stacklessError.WithStack()
+}
+
+func Test_Stack(t *testing.T) {
+	err := stacklessErrorReturningFunction()
+	assert.Empty(t, err.(*tagerr.Err).Stack())
+
+	err = stackfullErrorerrorReturningFunction()
+	assert.NotContains(t, err.(*tagerr.Err).Stack(), "github.com/pedramktb/go-tagerr.(*Err).WithStack")
+	assert.NotContains(t, err.(*tagerr.Err).Stack(), "/err.go:")
+	assert.Contains(t, err.(*tagerr.Err).Stack(), "github.com/pedramktb/go-tagerr_test.Test_Stack")
+	assert.Contains(t, err.(*tagerr.Err).Stack(), "github.com/pedramktb/go-tagerr_test.stackfullErrorerrorReturningFunction")
+	assert.Contains(t, err.(*tagerr.Err).Stack(), "/stack_test.go:")
+}
